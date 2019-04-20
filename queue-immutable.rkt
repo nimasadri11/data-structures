5eacdf1b7e72150bb90a1ca1712a7ac45c4844fa
Nima Sadri <nimasadri11@gmail.com>
2019-04-20 11:54:04 -0400

    imperative queue

---
+++ queue-immutable.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(provide empty-q empty-q? enq deq)
+
+;; pure functional implementation of an immutable Queue
+
+(struct q (front back))
+
+(define empty-q (q empty empty))
+
+(define (empty-q? q) (and (q? q) (empty? (q-front q)) (empty? (q-back q))))
+
+(define (enq que el) (q (cons el (q-front que)) (q-back que)))
+
+(define (deq que) (cond
+                    [(empty-q? q) que]
+                    [(not (empty? (q-back que))) (q (q-front que) (cdr (q-back que)))]
+                    [true (q empty (cdr (reverse (q-front que))))]))
+
+
